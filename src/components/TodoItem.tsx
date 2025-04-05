176542006da763be9481f939e910c6df7e8075b1
khskys0805 <khskys0805@naver.com>
2025-04-05 17:35:20 +0900

    feat: 투두 스타일 적용

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,4 +1,8 @@
 import { Todo } from "../types/todo";
+import {
+	MdOutlineCheckBox,
+	MdOutlineCheckBoxOutlineBlank,
+} from "react-icons/md";
 
 interface Props {
 	todo: Todo;
@@ -8,9 +12,27 @@ interface Props {
 
 const TodoItem = ({ todo, onToggle, onRemove }: Props) => {
 	return (
-		<li>
-			<span onClick={onToggle}>{todo.text}</span>
-			<button onClick={onRemove}>삭제</button>
+		<li className="mt-3 max-w-xl mx-auto px-4 flex items-center gap-3">
+			<span onClick={onToggle} className="cursor-pointer">
+				{todo.done ? (
+					<MdOutlineCheckBox />
+				) : (
+					<MdOutlineCheckBoxOutlineBlank />
+				)}
+			</span>
+			<span
+				className={`flex-grow text-left ${
+					todo.done ? "line-through text-gray-400" : ""
+				}`}
+			>
+				{todo.text}
+			</span>
+			<button
+				className="bg-[#3f3f3f] text-white px-3 py-2 rounded-md whitespace-nowrap"
+				onClick={onRemove}
+			>
+				삭제
+			</button>
 		</li>
 	);
 };
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -13,21 +13,23 @@ const Home = () => {
 	};
 	return (
 		<div className="mt-20">
-			<h1 className="text-3xl font-bold mb-10">✔️ ToDoList</h1>
-			<input
-				value={input}
-				onChange={(e) => setInput(e.target.value)}
-				placeholder="할 일을 적어주세요"
-				className="px-3 py-2 outline-none rounded-md border border-[#dbdbdb] w-100 mr-3"
-			/>
-			<button
-				onClick={handleAdd}
-				className="bg-[#3f3f3f] text-white px-3 py-2 rounded-md"
-			>
-				추가
-			</button>
+			<h1 className="text-3xl font-bold">✔️ ToDoList</h1>
+			<div className="mt-10 max-w-xl w-full mx-auto px-4 flex gap-3">
+				<input
+					value={input}
+					onChange={(e) => setInput(e.target.value)}
+					placeholder="할 일을 적어주세요"
+					className="flex-grow px-3 py-2 outline-none rounded-md border border-[#dbdbdb]"
+				/>
+				<button
+					onClick={handleAdd}
+					className="bg-[#3f3f3f] text-white px-3 py-2 rounded-md whitespace-nowrap"
+				>
+					추가
+				</button>
+			</div>
 
-			<ul>
+			<ul className="mt-10">
 				{todos.map((todo) => (
 					<TodoItem
 						key={todo.id}
